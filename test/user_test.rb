a0543852bb7f4d1a18846c63fd80a928f21e10c6
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 09:27:22 -0700

    Adds learn test to test file and joke requirement to user file

--- test/user_test.rb
+++ test/user_test.rb
@@ -18,4 +18,12 @@ class UserTest < Minitest::Test
     user = User.new("Sal")
     assert_equal [], user.jokes
   end
+
+  def test_it_can_learn_jokes
+    user = User.new("Sal")
+    assert_equal [], user.jokes
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    user.learn(joke)
+    assert_equal [joke], user.jokes
+  end
 end
