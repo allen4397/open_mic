4bd43f09eea519e74dae3b1df4f1a16f1d539cb8
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 10:24:18 -0700

    Adds test it can recognize repeated jokes

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -32,4 +32,17 @@ class OpenMicTest < Minitest::Test
     open_mic.welcome(ali)
     assert_equal [sal, ali], open_mic.performers
   end
+
+  def test_it_can_recognize_repeated_jokes
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    ali.learn(joke_1)
+    refute open_mic.repeated_jokes?
+    ali.tell(sal, joke_1)
+    assert open_mic.repeated_jokes
+  end
 end
