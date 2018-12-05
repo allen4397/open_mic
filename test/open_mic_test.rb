510b0f8ba9bd9d927437dc5bc8f68a2955c34bcd
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 10:01:42 -0700

    test it has a location

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -5,7 +5,12 @@ require './lib/open_mic'
 
 class OpenMicTest < Minitest::Test
   def test_it_exists
-    open_mic = OpenMic.new
+    open_mic = OpenMic.new({location: "Comedy Works"})
     assert_instance_of OpenMic, open_mic
+  end
+
+  def test_it_has_a_location
+    open_mic = OpenMic.new({location: "Comedy Works"})
+    assert_equal "Comedy Works", open_mic.location
   end
 end
