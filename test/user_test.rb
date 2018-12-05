929380912ba275f5d050f5c4db9c0dfbf434b02b
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 09:22:19 -0700

    Adds user name and test

--- test/user_test.rb
+++ test/user_test.rb
@@ -5,7 +5,12 @@ require './lib/user'
 
 class UserTest < Minitest::Test
   def test_it_exists
-    user = User.new
+    user = User.new("Sal")
     assert_instance_of User, user
+  end
+
+  def test_it_has_a_name
+    user = User.new("Sal")
+    assert_equal "Sal", user.name
   end
 end
