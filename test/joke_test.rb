91e28358d5515e15f59dd663c5714f99020fda08
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 09:16:18 -0700

    Adds tests for intstance, id, setup, punchline

---
+++ test/joke_test.rb
@@ -0,0 +1,26 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/joke'
+
+class JokeTest < Minitest::Test
+  def test_it_exists
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    assert_instance_of Joke, joke
+  end
+
+  def test_it_has_an_ID
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    assert_equal 1, joke.id
+  end
+
+  def test_it_has_a_setup
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    assert_equal "Why did the strawberry cross the road?", joke.setup
+  end
+
+  def test_it_has_a_punchline
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    assert_equal "Because his mother was in a jam.", joke.punchline
+  end
+end
