47198ae123c13242d475f30712107dde523b406c
Tim Allen <tnelsallen3@gmail.com>
2018-12-05 10:09:48 -0700

    Adds test it has performers

--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -5,17 +5,24 @@ require './lib/open_mic'
 
 class OpenMicTest < Minitest::Test
   def test_it_exists
-    open_mic = OpenMic.new({location: "Comedy Works"})
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     assert_instance_of OpenMic, open_mic
   end
 
   def test_it_has_a_location
-    open_mic = OpenMic.new({location: "Comedy Works"})
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     assert_equal "Comedy Works", open_mic.location
   end
 
   def test_it_has_a_date
     open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
     assert_equal "11-20-18", open_mic.date
+  end
+
+  def test_it_has_performers
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    assert_equal [], open_mic.performers
+    # sal = User.new("Sal")
+    # ali = User.new("Ali")
   end
 end
